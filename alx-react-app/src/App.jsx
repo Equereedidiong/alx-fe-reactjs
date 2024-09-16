3d9c87d6d6fc4e5887d119432c96ffea24a0493c
A'DI <EquereAedidiong@gmail.com>
2024-09-16 07:10:46 +0100

    Import commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+        Create a new file called WelcomeMessage.jsx under src/components
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,7 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <WelcomeMessage/>
+      <WelcomeMessage />
     </>
   )
 }
