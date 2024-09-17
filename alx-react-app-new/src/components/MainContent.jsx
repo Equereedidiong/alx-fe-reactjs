44a4d9d135af2b303b941c4a2c3e7dc635e555a7
A'DI <EquereAedidiong@gmail.com>
2024-09-17 16:58:59 +0100

    second commit

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent () {
+    return (
+        <main>
+           <p style = {{ fontWeight: 'bold', backgroundColor: 'red'}}>I love to visit New York, Paris, and Tokyo.</p>
+       </main>
+    )
+}
+
+export default MainContent;
---
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -0,0 +1,12 @@
+
+const UserProfile = (props) => {
+    return (
+      <div style = {{ border: '3px solid gray', padding: '10px', margin: '10px'}}>
+        <h2 style = {{ color: 'yellow'}}>{props.name}</h2>
+        <p>Age: <span style = {{ fontWeight: 'bold' }}></span> {props.age}</p>
+        <p>Bio: {props.bio}</p>
+      </div>
+    );
+  };
+
+  export default UserProfile;
