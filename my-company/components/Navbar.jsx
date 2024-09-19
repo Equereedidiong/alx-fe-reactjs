be1337decc002ec1f10201f1bcd4fa0f2b910be1
A'DI <EquereAedidiong@gmail.com>
2024-09-19 13:16:18 +0100

    Check now

---
+++ my-company/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+
+function Navbar () {
+
+    return (
+        <div>
+            <a href="/">Home</a>
+            <a href="/About">About Page</a>
+            <a href="/Contact">Contact Page</a>
+            <a href="/Services">Services Page</a>
+        </div>
+    )
+}
