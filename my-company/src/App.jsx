8d081ed48c7068530ab6f7951c19b141ab917b91
A'DI <EquereAedidiong@gmail.com>
2024-09-19 13:12:14 +0100

    Final trial

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -11,57 +11,72 @@ import Navbar from '../Navbar'
 
 
 
-
-const router = createBrowserRouter ([
-  {
-      path: "/",
-      Component: "HomePage",
-  },
-
-  {
-      path: "/About",
-      Component: "About",
-  },
-
-  {
-      path: "/Services",
-      Component: "Services",
-  },
-
-  {
-      path: "/Contact",
-      Component: "Contact",
-  },
-
-])
-
-function App() {
-  const [count, setCount] = useState(0)
-
+function App () {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <Router>
+     <Routes>
+      
+         <Route path="/" element={<Home />} />
+         <Route path="/About" element={<About />} />
+         <Route path="/Services" element={<Services />} />
+         <Route path="/Contact" element={<Contact />} />
+  
+      </Routes>
+  </Router>
+  
   )
-}
+  
+  }
+// const router = createBrowserRouter ([
+//   {
+//       path: "/",
+//       Component: "HomePage",
+//   },
 
-export default App
+//   {
+//       path: "/About",
+//       Component: "About",
+//   },
+
+//   {
+//       path: "/Services",
+//       Component: "Services",
+//   },
+
+//   {
+//       path: "/Contact",
+//       Component: "Contact",
+//   },
+
+// ])
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
