ef769583d2704c4520995dcee7a591788ffa6026
A'DI <EquereAedidiong@gmail.com>
2024-09-19 13:07:10 +0100

    Just this now

--- my-company/App.jsx
+++ my-company/App.jsx
@@ -2,30 +2,45 @@ import Home from './Home'
 import About from './About'
 import Services from './Services'
 import Contact from './Contact'
-import { createBrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowseRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './Navbar'
 
+function App () {
+return (
+<Router>
+   <Routes>
+    
+       <Route path="/" element={<Home />} />
+       <Route path="/About" element={<About />} />
+       <Route path="/Services" element={<Services />} />
+       <Route path="/Contact" element={<Contact />} />
 
-const router = createBrowserRouter ([
-    {
-        path: "/",
-        Component: "HomePage",
-    },
+    </Routes>
+</Router>
 
-    {
-        path: "/About",
-        Component: "About",
-    },
+)
 
-    {
-        path: "/Services",
-        Component: "Services",
-    },
+}
+// const router = createBrowserRouter ([
+//     {
+//         path: "/",
+//         Component: "HomePage",
+//     },
 
-    {
-        path: "/Contact",
-        Component: "Contact",
-    },
+//     {
+//         path: "/About",
+//         Component: "About",
+//     },
 
-])
+//     {
+//         path: "/Services",
+//         Component: "Services",
+//     },
+
+//     {
+//         path: "/Contact",
+//         Component: "Contact",
+//     },
+
+// ])
 
