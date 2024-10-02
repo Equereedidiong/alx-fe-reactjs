56ecf5f947c304d9cd2f762c4a5dbdc78288acf1
A'DI <EquereAedidiong@gmail.com>
2024-10-02 05:29:19 +0100

    ok

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        < AddRecipeForm />
+      </div>
+
+      <div>
+        < RecipeList />
+      </div>
+    </>
+  )
+}
+
+export default App
