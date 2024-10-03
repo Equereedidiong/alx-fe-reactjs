19f814745a8dcf749acfd5c826de1baa7cbee2d5
A'DI <EquereAedidiong@gmail.com>
2024-10-03 06:01:32 +0100

    Im tired

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import useRecipeStore  from './recipeStore';
+import { useNavigate  } from 'react-router-dom';
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
-
+  useN
   const handleDelete = () => {
     deleteRecipe(recipeId);
     // You can add a redirect here after deleting, if needed
